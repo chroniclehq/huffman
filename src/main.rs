4e7f6a281c05ea12ec21d772aa3cc53c53e7d65b
harris <harris@chroniclehq.com>
2022-07-21 12:35:31 +0530

    Add initial version of optimizer

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::tokio::fs::File;
+use rocket::tokio::io::AsyncReadExt;
+use std::io::Error;
+use std::path::{Path, PathBuf};
+
+mod services;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Responder)]
+#[response(content_type = "image/webp")]
+struct ImageResponse(Vec<u8>);
+
+#[get("/library/<file..>")]
+async fn test(file: PathBuf) -> Option<ImageResponse> {
+    let path: PathBuf =
+        Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/test/")).join(file);
+
+    println!("{}", path.as_os_str().to_string_lossy());
+
+    let res: Result<File, Error> = File::open(path).await;
+
+    match res {
+        Ok(file) => {
+            let mut file: File = file.try_clone().await.unwrap();
+            let mut original_image: Vec<u8> = Vec::new();
+
+            let size = file.read_to_end(&mut original_image).await.unwrap();
+
+            let result: Result<Vec<u8>, libvips::error::Error> =
+                services::image::process_image(&original_image);
+
+            match result {
+                Ok(optimised_image) => {
+                    println!("Optimised");
+                    Some(ImageResponse(optimised_image))
+                }
+                Err(error) => {
+                    println!("{}", error);
+                    Some(ImageResponse(original_image))
+                }
+            }
+        }
+        Err(error) => {
+            println!("{}", error);
+            None
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![test])
+}
---
+++ src/services/image/mod.rs
@@ -0,0 +1,14 @@
+use libvips::ops;
+use libvips::ops::webpsave_buffer_with_opts;
+use libvips::Result;
+use libvips::VipsImage;
+
+pub fn process_image(buffer: &Vec<u8>) -> Result<Vec<u8>> {
+    let source = VipsImage::new_from_buffer(buffer, "").expect("Error during VipsImage init");
+    let options = ops::WebpsaveBufferOptions {
+        q: 50,
+        strip: true,
+        ..ops::WebpsaveBufferOptions::default()
+    };
+    webpsave_buffer_with_opts(&source, &options)
+}
