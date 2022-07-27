79560d1a544ba35ce179831eb4b2bf5cf2fcd657
harris <harris@chroniclehq.com>
2022-07-27 11:49:32 +0530

    Add cors fairing to support fetching images via xhr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
 use rocket::tokio::fs::File;
 use rocket::tokio::io::AsyncReadExt;
+use rocket::{Request, Response};
 use std::io::Error;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 
 mod services;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    // https://stackoverflow.com/a/64904947
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "GET, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -60,6 +82,7 @@ async fn optimize(file: PathBuf) -> Option<ImageResponse> {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .mount("/", routes![index])
         .mount("/", routes![optimize])
 }
