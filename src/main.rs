58f016a3dc698211556e22a66ae943cbe92b5ac5
praveen kumar <10971670+praveendinesh6@users.noreply.github.com>
2022-07-28 14:20:00 +0530

    generate and fetch image flow handling

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ extern crate dotenv;
 use dotenv::dotenv;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
+use rocket::http::Status;
 use rocket::tokio::fs::File;
 use rocket::tokio::io::AsyncReadExt;
 use rocket::{Request, Response};
 use std::env;
+use std::fs;
 use std::io::Error;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
@@ -84,6 +86,138 @@ async fn test(file: PathBuf) -> Option<ImageResponse> {
     }
 }
 
+#[get("/library/<file..>")]
+async fn generate(file: PathBuf) -> Status {
+    let path: PathBuf =
+        Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/test/")).join(&file);
+
+    println!("{}", path.as_os_str().to_string_lossy());
+
+    let res: Result<File, Error> = File::open(path).await;
+
+    match res {
+        Ok(original_file) => {
+            let mut original_file: File = original_file.try_clone().await.unwrap();
+            let mut original_image: Vec<u8> = Vec::new();
+
+            let size = original_file
+                .read_to_end(&mut original_image)
+                .await
+                .unwrap();
+
+            let result: Result<Vec<u8>, libvips::error::Error> =
+                services::image::process_image(&original_image);
+
+            match result {
+                Ok(optimised_image) => {
+                    println!("Optimised");
+
+                    let (file_name_without_ext, _) =
+                        file.to_str().unwrap().split_once('.').unwrap();
+                    let dest_location = format!(
+                        "{}/src/assets/test/updated/{}.webp",
+                        env!("CARGO_MANIFEST_DIR"),
+                        file_name_without_ext
+                    );
+                    let dest_path: &Path = Path::new(&dest_location);
+                    fs::create_dir_all(dest_path.parent().unwrap()).unwrap();
+                    fs::write(dest_location, &optimised_image).unwrap();
+                    Status::Ok
+                }
+                Err(error) => {
+                    println!("{}", error);
+                    Status::InternalServerError
+                }
+            }
+        }
+        Err(error) => {
+            println!("{}", error);
+            Status::InternalServerError
+        }
+    }
+}
+
+#[get("/library/<file..>")]
+async fn fetch(file: PathBuf) -> Option<ImageResponse> {
+    let (file_name_without_ext, _) = file.to_str().unwrap().split_once('.').unwrap();
+    let optimised_image_location = format!(
+        "{}/src/assets/test/updated/{}.webp",
+        env!("CARGO_MANIFEST_DIR"),
+        file_name_without_ext
+    );
+    let optimised_image_path: &Path = Path::new(&optimised_image_location);
+
+    if optimised_image_path.exists() {
+        let res: Result<File, Error> = File::open(optimised_image_path).await;
+
+        match res {
+            Ok(original_file) => {
+                println!("file exists. Returning cached image");
+
+                let mut original_image: Vec<u8> = Vec::new();
+                original_file
+                    .try_clone()
+                    .await
+                    .unwrap()
+                    .read_to_end(&mut original_image)
+                    .await
+                    .unwrap();
+                Some(ImageResponse(original_image))
+            }
+            Err(error) => {
+                println!("Error while reading file from cache.  Error is {}", error);
+                None
+            }
+        }
+    } else {
+        let path: PathBuf =
+            Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/test/")).join(&file);
+
+        let res: Result<File, Error> = File::open(path).await;
+
+        match res {
+            Ok(original_file) => {
+                let mut original_file: File = original_file.try_clone().await.unwrap();
+                let mut original_image: Vec<u8> = Vec::new();
+
+                let size = original_file
+                    .read_to_end(&mut original_image)
+                    .await
+                    .unwrap();
+
+                let result: Result<Vec<u8>, libvips::error::Error> =
+                    services::image::process_image(&original_image);
+
+                match result {
+                    Ok(optimised_image) => {
+                        println!("Optimised");
+
+                        let (file_name_without_ext, _) =
+                            file.to_str().unwrap().split_once('.').unwrap();
+                        let dest_location = format!(
+                            "{}/src/assets/test/updated/{}.webp",
+                            env!("CARGO_MANIFEST_DIR"),
+                            file_name_without_ext
+                        );
+                        let dest_path: &Path = Path::new(&dest_location);
+                        fs::create_dir_all(dest_path.parent().unwrap()).unwrap();
+                        fs::write(dest_location, &optimised_image).unwrap();
+                        Some(ImageResponse(optimised_image))
+                    }
+                    Err(error) => {
+                        println!("{}", error);
+                        Some(ImageResponse(original_image))
+                    }
+                }
+            }
+            Err(error) => {
+                println!("{}", error);
+                None
+            }
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
@@ -91,4 +225,6 @@ fn rocket() -> _ {
         .attach(CORS)
         .mount("/", routes![index])
         .mount("/", routes![test])
+        .mount("/generate", routes![generate])
+        .mount("/fetch", routes![fetch])
 }
