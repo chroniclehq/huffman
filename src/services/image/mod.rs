90f9870b273a5dbf82b0601b291d10aa17e928b6
praveen kumar <10971670+praveendinesh6@users.noreply.github.com>
2022-08-05 19:21:00 +0530

    Skipping file generation if the variant is available in cache

--- src/services/image/mod.rs
+++ src/services/image/mod.rs
@@ -14,10 +14,11 @@ pub enum Variants {
 }
 
 pub fn get_variant_path(variant: Variants) -> String {
+    let folder_name = "image_optimizer";
     let paths: EnumMap<Variants, &str> = enum_map! {
         Variants::Default => "default"
     };
-    paths[variant].to_string()
+    format!("{}/{}", folder_name, paths[variant].to_string())
 }
 
 pub fn optimize(buffer: &Vec<u8>) -> libvips::Result<Vec<u8>> {
@@ -32,30 +33,44 @@ pub fn optimize(buffer: &Vec<u8>) -> libvips::Result<Vec<u8>> {
 }
 
 pub async fn generate(key: &str, storage: &Storage) -> anyhow::Result<()> {
-    let image = storage.read(key).await?;
-    let result: Result<Vec<u8>, libvips::error::Error> = optimize(&image);
+    let file_name_without_ext = utils::get_path_without_ext(key);
+    let variant_path = get_variant_path(Variants::Default);
 
-    match result {
-        Ok(optimised_image) => {
-            let file_name_without_ext = utils::get_path_without_ext(key);
-            let variant_path = get_variant_path(Variants::Default);
-            let target_key = format!("{}/{}.webp", variant_path, file_name_without_ext);
+    let target_path = format!("{}/{}.webp", variant_path, file_name_without_ext);
+    let cached_image = storage.read_from_cache(&target_path).await;
 
-            storage
-                .write(
-                    &target_key,
-                    UploadData {
-                        content_type: ContentType::WEBP,
-                        body: optimised_image,
-                    },
-                )
-                .await?;
-
+    match cached_image {
+        Ok(_) => {
+            println!("Variant found for {}. Skipping generate flow.", key);
             Ok(())
         }
-        Err(error) => Err(anyhow::anyhow!(format!(
-            "Error during optimization. Key: {}, Error: {:?}",
-            key, error
-        ))),
+        Err(_error) => {
+            let image = storage.read(key).await?;
+            let result: Result<Vec<u8>, libvips::error::Error> = optimize(&image);
+
+            match result {
+                Ok(optimised_image) => {
+                    let file_name_without_ext = utils::get_path_without_ext(key);
+                    let variant_path = get_variant_path(Variants::Default);
+                    let target_key = format!("{}/{}.webp", variant_path, file_name_without_ext);
+
+                    storage
+                        .write(
+                            &target_key,
+                            UploadData {
+                                content_type: ContentType::WEBP,
+                                body: optimised_image,
+                            },
+                        )
+                        .await?;
+
+                    Ok(())
+                }
+                Err(error) => Err(anyhow::anyhow!(format!(
+                    "Error during optimization. Key: {}, Error: {:?}",
+                    key, error
+                ))),
+            }
+        }
     }
 }
