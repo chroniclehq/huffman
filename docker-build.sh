5ad06bb51502d1edd2621d27043945b9efd10a67
harris <harris@chroniclehq.com>
2022-08-08 23:03:44 +0530

    Update docker build scripts with platform

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # TODO: Get these from commit hash
-VERSION="0.1"
+VERSION="0.2"
 REGISTRY="ghcr.io/chroniclehq"
 
-docker build -f Dockerfile.build -t $REGISTRY/huffman-build:$VERSION . && \
-docker build -f Dockerfile.runtime --build-arg VERSION=$VERSION --build-arg REGISTRY=$REGISTRY -t $REGISTRY/huffman-runtime:$VERSION .
+docker build -f Dockerfile.build --platform=linux/amd64 -t $REGISTRY/huffman-build:$VERSION . && \
+docker build -f Dockerfile.runtime --platform=linux/amd64 --build-arg VERSION=$VERSION --build-arg REGISTRY=$REGISTRY -t $REGISTRY/huffman-runtime:$VERSION .
+
+docker tag $REGISTRY/huffman-build:$VERSION $REGISTRY/huffman-build:latest
+docker tag $REGISTRY/huffman-runtime:$VERSION $REGISTRY/huffman-runtime:latest
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # TODO: Get these from commit hash
-VERSION="0.1"
+VERSION="0.2"
 REGISTRY="ghcr.io/chroniclehq"
 
 docker push $REGISTRY/huffman-build:$VERSION && \
-docker push $REGISTRY/huffman-runtime:$VERSION
+docker push $REGISTRY/huffman-build:latest && \
+docker push $REGISTRY/huffman-runtime:$VERSION && \
+docker push $REGISTRY/huffman-runtime:latest
+
